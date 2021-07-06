9197c5f567197bc4dbfd8c51960147ce386d2b51
Omar Ahmed ElSayed <omarahmed8k@gmail.com>
2021-07-06 15:41:35 +0200

    Create Day2 - Omar Ahmed Elsayed.cs

---
+++ Day2 - Omar Ahmed Elsayed.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            // Day 2 - Lab - Omar Ahmed Elsayed
+
+            #region Task 1
+
+            //Console.WriteLine("Enter The Character");
+            //char x = char.Parse(Console.ReadLine());
+            //int y = (int)x;
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #region Task 2
+
+            //Console.WriteLine("Enter The Number");
+            //int x = int.Parse(Console.ReadLine());
+            //char y = (char)x;
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #region Task 3
+
+            //Console.WriteLine("Enter The Number");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine("Even");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Odd");
+            //}
+
+
+            #endregion
+
+            #region Task 4
+
+            //Console.WriteLine("Enter The First Number");
+            //int x = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter The Second Number");
+            //int y = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Sum = {x + y} , Mul = {x * y} , Div = {x / y} , Sub = {x - y}");
+
+            #endregion
+
+            #region Task 5
+
+            //Console.WriteLine("Enter The Degree");
+            //int x = int.Parse(Console.ReadLine());
+            //if (x >= 85 && x <= 100)
+            //{
+            //    Console.WriteLine("A");
+            //}
+            //else if (x >= 75 && x < 85)
+            //{
+            //    Console.WriteLine("B");
+            //}
+            //else if (x >= 65 && x < 75)
+            //{
+            //    Console.WriteLine("C");
+            //}
+            //else if (x >= 50 && x < 65)
+            //{
+            //    Console.WriteLine("D");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("F");
+            //}
+
+            #endregion
+
+            #region Task 6
+
+            //for (int x = 1; x < 10; x++)
+            //{
+            //    for (int y = 1; y < 11; y++)
+            //    {
+            //        int z = x * y;
+            //        Console.WriteLine($"{x} * {y} = {z}");
+            //    }
+            //    Console.WriteLine("-------------");
+            //}
+
+            #endregion
+        }
+    }
+}
